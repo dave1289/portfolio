5f8962cb051a7c2c0ff7a5664ec26004a21b7fc5
dave <davemcelhaney@gmail.com>
2022-12-18 23:25:59 -0500

    created dynamic pages via jquery for the content of the page

---
+++ app.js
@@ -0,0 +1,30 @@
+$('document').ready(function(){
+    $('#meet-me').on('click', ()=>{
+        // console.log('test-about-me')
+        $('#main-page').hide();
+        $('#contact-page').hide();
+        $('#my-work-page').hide();
+        $('#about-page').show()
+    })
+    $('#my-work').on('click', ()=>{
+        // console.log('test-my-work')
+        $('#main-page').hide();
+        $('#my-work-page').show();
+        $('#contact-page').hide();
+        $('#about-page').hide()
+    })
+    $('#contact').on('click', ()=>{
+        // console.log('test-contact')
+        $('#main-page').hide();
+        $('#contact-page').show();
+        $('#about-page').hide();
+        $('#my-work-page').hide();
+    })
+    $('#home-page-dave').on('click', ()=>{
+        // console.log('test dave')
+        $('#about-page').hide();
+        $('#main-page').show();
+        $('#contact-page').hide();
+        $('#my-work-page').hide();
+    })
+})
